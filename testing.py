5c23d073200741e2ed1a1d82fa7842546e9093ae
Naufal Zakly Santoso <72109817+naufalzakly@users.noreply.github.com>
2021-11-28 21:41:54 +0700

    menghapus def test_MIT pada file testing.py

--- testing.py
+++ testing.py
@@ -3,11 +3,7 @@ from unittest import result
 import suhu_testingLibrary
 
 class TestMSI(unittest.TestCase):
-    
-    def test_MIT(self):
-        result = suhu_testingLibrary.massaIndexTubuh (60,2.0)
-        self.assertEqual(result, 15.0)
-    
+
     def test_Faranheit(self):
         result_Faranheit = suhu_testingLibrary.suhuConvertFaranheit(30)
         self.assertEqual(result_Faranheit,86 )
