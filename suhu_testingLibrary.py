c28dd0db7f55e97648d355e3faf1dcf1dc884098
Naufal Zakly Santoso <72109817+naufalzakly@users.noreply.github.com>
2021-12-01 21:35:54 +0700

    update suhu_testingLibrary.py

--- suhu_testingLibrary.py
+++ suhu_testingLibrary.py
@@ -2,42 +2,17 @@
 def suhuConvertFaranheit(C):
     F = (C * (9/5)) + 32 #rumus faranhite
     return F
-suhuConvertFaranheit(30)
+faranheit = suhuConvertFaranheit(30)
+print ("Suhu Faranheit {}".format(faranheit))
 # fungsi untuk Suhu ke Kelvin
 def suhuConvertKelvin(C):
     K = C + 273.15 #rumus kelvin
     return K
-suhuConvertKelvin(30)
+kelvin = suhuConvertKelvin(30)
+print ("Suhu Kelvin {}".format(kelvin))
 # fungsi untuk Suhu ke Reamur
 def suhuConvertReamur(C):
     Re = C * (4/5) #rumus reamur
     return Re
-suhuConvertReamur(30)
-# fungsi untuk menampilkan menu
-def show_menu():
-    print ("\n")
-    print ("----------- MENU ----------")
-    print ("[1] Mengubah Suhu ke Faranheit")
-    print ("[2] Mengubah Suhu ke Reamur")
-    print ("[3] Mengubah Suhu ke Kelvin")
-    print ("[4] Exit")
-    
-    menu = int(input("PILIH MENU> "))
-    print ("\n")
-
-    if menu == 1:
-        suhuConvertFaranheit()
-    elif menu == 2:
-        suhuConvertReamur()
-    elif menu == 3:
-        suhuConvertKelvin()
-    elif menu == 4:
-        exit()
-    else:
-        print ("Salah pilih!")
-
-
-if __name__ == "__main__":
-
-    while(True):
-        show_menu()
+reamur = suhuConvertReamur(30)
+print ("Suhu Reamur {}".format(reamur))
