9232136a027f409f2c3d9a010cc9ad73627948ef
Naufal Zakly Santoso <72109817+naufalzakly@users.noreply.github.com>
2021-12-02 00:39:45 +0700

    update suhu_testingLibrary.py

--- suhu_testingLibrary.py
+++ suhu_testingLibrary.py
@@ -1,18 +1,50 @@
-# fungsi untuk Suhu ke Faranheit
-def suhuConvertFaranheit(C):
-    F = (C * (9/5)) + 32 #rumus faranhite
-    return F
-faranheit = suhuConvertFaranheit(30)
-print ("Suhu Faranheit {}".format(faranheit))
-# fungsi untuk Suhu ke Kelvin
-def suhuConvertKelvin(C):
-    K = C + 273.15 #rumus kelvin
-    return K
-kelvin = suhuConvertKelvin(30)
-print ("Suhu Kelvin {}".format(kelvin))
-# fungsi untuk Suhu ke Reamur
-def suhuConvertReamur(C):
-    Re = C * (4/5) #rumus reamur
-    return Re
-reamur = suhuConvertReamur(30)
-print ("Suhu Reamur {}".format(reamur))
+    
+class Suhuu:
+    
+    # fungsi untuk Suhu ke Faranheit
+    def CelciusConvertFaranheit(self):
+        C = float(input("Masukan suhu Celcius untuk diconvert ke Faranheit: "))
+        F = (C * (9/5)) + 32 #rumus faranhite
+        print("Suhu Celcius dari",C, "°C"," adalah ",F,"°F")
+        
+        
+        
+    # fungsi untuk Suhu ke Kelvin
+    def CelciusConvertKelvin(self):
+        C = float(input("Masukan suhu Celcius untuk diconvert ke Kelvin: "))
+        K = C + 273.15 #rumus kelvin
+        print ("Suhu Celcius dari",C,"°C", " adalah ",K,"°K")
+        
+        
+
+    # fungsi untuk Suhu ke Reamur
+    def CelciusConvertReamur(self):
+        C = float(input("Masukan suhu Celcius untuk diconvert ke Reamur: "))
+        Re = C * (4/5) #rumus reamur
+        print ("Suhu Celcius dari",C,"°C", " adalah ",Re,"°Re")
+        
+        
+    
+    def show_menu(self):
+        print ("\n")
+        print ("----------- MENU ----------")
+        print ("[1] Celcius Convert Faranheit")
+        print ("[2] Celcius Convert Kelvin")
+        print ("[3] Celcius Convert Reamur")
+        print ("[4] Exit")
+        
+        menu = int(input("PILIH MENU: "))
+        print ("\n")
+
+        if menu == 1:
+            self.CelciusConvertFaranheit()
+        elif menu == 2:
+            self.CelciusConvertKelvin()
+        elif menu == 3:
+            self.CelciusConvertReamur()
+        elif menu == 4:
+            exit()
+        else:
+            print ("Salah pilih!")
+play = Suhuu()
+play.show_menu()
